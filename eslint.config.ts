05eba35c401393cf3fd03868e15d10a797a8fbbd
Raine <randomrandom.epic.epic@gmail.com>
2026-02-22 14:11:56 -0500

    fix(dev): fix markdown linting

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,29 @@
 import isentinel, { type TypedFlatConfigItem } from "@isentinel/eslint-config";
 
-export default isentinel(
+const config = isentinel(
 	{
 		name: "project/root",
-		namedConfigs: true,
-		pnpm: true,
-		react: true,
-		roblox: true,
-
 		// disabled due to genuinely insane time taken for little benefit
 		formatters: {
 			lua: false,
 		},
+		namedConfigs: true,
+		pnpm: true,
+		react: true,
+
+		roblox: true,
 		spellCheck: false,
 	},
+
+	// {
+	// 	files: ["**/*.md"],
+	// 	name: "project/markdown-perfectionist",
+	// 	rules: {
+	// 		"perfectionist/sort-imports": "off",
+	// 	},
+	// } satisfies TypedFlatConfigItem,
 	{
+		// ignores: ["**.md"],
 		name: "project/sort",
 		rules: {
 			"perfectionist/sort-classes": [
@@ -23,18 +32,29 @@ export default isentinel(
 					partitionByNewLine: false,
 				},
 			],
-			"perfectionist/sort-imports": [
+			"perfectionist/sort-enums": [
 				"error",
 				{
-					partitionByNewLine: false,
+					partitionByNewLine: true,
+				},
+			],
+			"perfectionist/sort-objects": [
+				"error",
+				{
+					partitionByNewLine: true,
 				},
 			],
 		},
+
 		settings: {
 			perfectionist: {
 				partitionByComment: true,
-				partitionByNewLine: true,
+				// would be nice to put it here but it destroys everything for
+				// markdown files, perfectionist issue with sort-imports :c
+				// partitionByNewLine: true,
 			},
 		},
 	} satisfies TypedFlatConfigItem,
 );
+
+export default config;
